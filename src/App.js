fd651be1f18df30741f28e1d1108d1721d8fad53
nickzylstra <nick.zylstra@gmail.com>
2020-03-07 17:36:04 -0800

    Refactor for organization

--- src/App.js
+++ src/App.js
@@ -11,28 +11,29 @@ const D3Container = ({ data }) => {
     const svg = d3.select(d3Container.current);
     const trans = svg.transition().duration(1200);
 
+    const joinFns = {
+      enter: (enter) => enter.append("text")
+          .attr('fill', 'blue')
+          .attr('x', -width)
+          .attr('y', (d, i) => i * height + height)
+          .style('font-size', height)
+          .text((d) => d)
+        .call((enter) => enter.transition(trans)
+          .attr('x', 0)),
+      update: (update) => update
+          .attr('fill', 'black')
+        .call((update) => update.transition(trans)
+          .attr('y', (d, i) => i * height + height)),
+      exit: (exit) => exit
+          .attr('fill', 'red')
+        .call((exit) => exit.transition(trans)
+          .attr('x', width)
+          .remove()),
+    }
 
     svg.selectAll("text")
       .data(data, (d) => d)
-      .join(
-        (enter) => enter.append("text")
-            .attr('fill', 'blue')
-            .attr('x', -width)
-            .attr('y', (d, i) => i * height + height)
-            .style('font-size', height)
-            .text((d) => d)
-          .call((enter) => enter.transition(trans)
-            .attr('x', 0)),
-        (update) => update
-            .attr('fill', 'black')
-          .call((update) => update.transition(trans)
-            .attr('y', (d, i) => i * height + height)),
-        (exit) => exit
-            .attr('fill', 'red')
-          .call((exit) => exit.transition(trans)
-            .attr('x', width)
-            .remove())
-      );
+      .join(joinFns.enter, joinFns.update, joinFns.exit);
   }, [data]);
 
   return (
@@ -46,22 +47,18 @@ const D3Container = ({ data }) => {
 };
 
 function App() {
-  const numberCount = 20;
-  const genNumbers = () => {
+  const genNumbers = (count) => {
+    const numberCount = 20;
     const numbers = Array(numberCount).fill().map((e, i) => i);
-    const shuffledNumbers = d3.shuffle(numbers).slice(0, Math.floor(Math.random() * numberCount)).sort((a,b) => a - b);
-    return shuffledNumbers;
+    const shuffledNumbersSubset = d3.shuffle(numbers).slice(0, Math.floor(Math.random() * numberCount)).sort((a,b) => a - b);
+    return shuffledNumbersSubset;
   } 
-  const [data, setData] = useState(genNumbers);
+  const [data, setData] = useState(genNumbers());
 
   useEffect(() => {
-    const refreshInterval = setInterval(() => {
-      setData(genNumbers())
-    }, 2500);
+    const refreshInterval = setInterval(() => setData(genNumbers()), 2500);
 
-    return () => {
-      clearInterval(refreshInterval);
-    };
+    return () => { clearInterval(refreshInterval); };
   })
 
   return (
